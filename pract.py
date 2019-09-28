473ad129123334355af5d5fd6e8565b221451391
Deore <ndeore@elogex.com>
2019-09-29 00:01:24 +0530

    none type exercise

---
+++ pract.py
@@ -0,0 +1,21 @@
+GRAND_TOTAL =1000
+
+def get_receieved_marks():
+    return int(input(
+        'Enter Received Marks: '
+    ))
+
+def calculate_percentage(recieved_marks):
+    if recieved_marks <= 0:
+        return None
+    else:
+        return (recieved_marks / GRAND_TOTAL ) * 100
+
+if __name__ == '__main__':
+    recieved_marks = get_receieved_marks()
+    perc = calculate_percentage(recieved_marks)
+
+    if perc is not None:
+        print('Percentage is %s' % str(perc))
+    else:
+        print('Input marks were invalid')
